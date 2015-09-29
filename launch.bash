14d33ab21b22dcb54a6cc1614a3485c666d52411
Jimmy Fan <fanminjian@gmail.com>
2015-09-29 23:39:00 +1000

    Add SSL Proxy

--- launch.bash
+++ launch.bash
@@ -14,6 +14,21 @@ fi
 
 xmlstarlet ed -u '//Context/@path' -v "$CONTEXT_PATH" conf/server-backup.xml > conf/server.xml
 
+if [ -n "$CONNECTOR_PROXYNAME" ]; then
+	xmlstarlet ed --inplace --delete "/Server/Service/Connector/@proxyName" conf/server.xml
+	xmlstarlet ed --inplace --insert "/Server/Service/Connector" --type attr -n proxyName -v $CONNECTOR_PROXYNAME conf/server.xml
+fi
+
+if [ -n "$CONNECTOR_PROXYPORT" ]; then
+	xmlstarlet ed --inplace --delete "/Server/Service/Connector/@proxyPort" conf/server.xml
+	xmlstarlet ed --inplace --insert "/Server/Service/Connector" --type attr -n proxyPort -v $CONNECTOR_PROXYPORT conf/server.xml
+fi
+
+if [ -n "$CONNECTOR_SECURE" ]; then
+	xmlstarlet ed --inplace --delete "/Server/Service/Connector/@secure" conf/server.xml
+	xmlstarlet ed --inplace --insert "/Server/Service/Connector" --type attr -n secure -v $CONNECTOR_SECURE conf/server.xml
+fi
+
 if [ -n "$DATABASE_URL" ]; then
   extract_database_url "$DATABASE_URL" DB /opt/jira/lib
   DB_JDBC_URL="$(xmlstarlet esc "$DB_JDBC_URL")"
